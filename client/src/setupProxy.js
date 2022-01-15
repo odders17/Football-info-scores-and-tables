ef43f7be43a0d2f7e078059448d7b92d778a2ccb
odders17 <odders17@gmail.com>
2022-01-15 11:47:56 +0000

    changed heroku link

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,19 +3,19 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     proxy('/api/auth', {
-      target: 'https://football-info-app.herokuapp.com',
+      target: 'https://dashboard.heroku.com/apps/football-info-app-odders17',
       changeOrigin: true
     })
   );
   app.use(
     proxy('/api/leagues', {
-      target: 'https://football-info-app.herokuapp.com',
+      target: 'https://dashboard.heroku.com/apps/football-info-app-odders17',
       changeOrigin: true
     })
   );
   app.use(
     proxy('/api/users', {
-      target: 'https://football-info-app.herokuapp.com',
+      target: 'https://dashboard.heroku.com/apps/football-info-app-odders17',
       changeOrigin: true
     })
   );
